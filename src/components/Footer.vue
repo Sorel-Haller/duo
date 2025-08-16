666fc888b8ff6f1f31e309aaadf27c131fccef36
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 00:09:35 +0300

    Added Contact info "Phone" and "Address" texts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,28 @@
 <template>
   <footer class="bg-[#001412] text-white px-8 md:px-32 py-16 flex flex-col">
     <!-- Top Section -->
-     <section class="h-[206px]">
+    <section class="h-[206px]">
         <h1 class="font-[Outfit] font-semibold text-[64px] md:text-[72px] lg:text-[100px] leading-[100%] tracking-[-0.02em]">
             Have an idea? <span class="font-normal underline underline-offset-10 decoration-[2px]">Let’s Talk</span>
         </h1>        
-     </section>
-         <div class="grid grid-cols-1 md:grid-cols-4 gap-12">
-      <!-- Contact -->
+    </section>
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-12">
+      <!-- Contact Info -->
       <div class="flex flex-col w-[273px] h-[284px] gap-5">
         <div class="flex flex-col gap-2">
             <h3 class="font-[Outfit] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Get in touch</h3>
             <p class="font-[Outfit] font-semibold text-[32px] leading-[42px] tracking-[-0.02em]">info@template.com</p>            
         </div>
+        <div class="flex flex-col gap-3">
+          <p class="font-[Supreme] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Phone</p>
+          <p class="font-[Supreme] font-medium text-[18px] leading-[23px] tracking-[-0.02em]">+000 111 222 3333 <br>+444 555 666 7777</p>
+        </div>
+        <div class="flex flex-col gap-3">
+          <p class="font-[Supreme] font-bold text-[20px] leading-[26px] tracking-[-0.02em]">Address</p>
+          <p class="font-[Supreme] font-bold text-[18px] leading-[23px] tracking-[-0.02em]">2352 Middlefield Rd, Markham <br> Ontario, Canada</p>
+        </div>
       </div>
     </div>
-
   </footer>
 </template>
 
