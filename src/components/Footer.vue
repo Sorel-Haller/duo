3362ae4fe2eb22859b26e82e0ca094078bd34f6a
Sorel-Haller <hallersorel@gmail.com>
2025-08-16 23:46:17 +0300

    Added footer background color and added top text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,1 +1,15 @@
-<template></template>
+<template>
+  <footer class="bg-[#001412] text-white px-8 md:px-32 py-16 flex flex-col">
+    <!-- Top Section -->
+     <section class="h-[206px]">
+        <h1 class="font-[Outfit] font-semibold text-[64px] md:text-[72px] lg:text-[100px] leading-[100%] tracking-[-0.02em]">
+            Have an idea? <span class="font-normal underline underline-offset-10 decoration-[2px]">Let’s Talk</span>
+        </h1>        
+     </section>
+     
+
+  </footer>
+</template>
+
+<script setup>
+</script>
