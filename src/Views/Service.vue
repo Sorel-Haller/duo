634758abf9009bdb4bea31e5bef7ae99b2ba1ed9
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 22:32:03 +0300

    Created seoService component and added it to Service.vue view. Added text and illustration to it

--- src/Views/Service.vue
+++ src/Views/Service.vue
@@ -3,6 +3,7 @@
     <WorkProcessService />
     <DesignProcessService />
     <DesignService />
+    <SeoService />
 </template>
 
 <script setup>
@@ -10,5 +11,6 @@
     import WorkProcessService from '../components/workProcessService.vue';
     import DesignProcessService from '../components/designProcessService.vue';
     import DesignService from '../components/designService.vue';
+    import SeoService from '../components/seoService.vue';
 
 </script>
---
+++ src/components/seoService.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
+        <div class="bg-[#CCEDE9] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
+            <div class="flex xl:flex-row flex-col gap-[40px] xl:items-center">
+                <!-- Body -->
+                <div class="flex flex-col gap-[35px] xl:w-[1026px]">
+                    <h2 class="font-outfit font-semibold text-[#004B49] lg:text-[64px] sm:text-[48px] text-[36px]">SEO</h2>
+                    <p class="font-outfit font-regular text-[#004B49] sm:text-[20px] text-[18px]">SEO allows you to climb the search engine results page rankings by improving on-page and off-page elements of your website that work together to make it more beneficial to your target audience. Higher rankings mean more site traffic and it’s no secret that more site traffic leads to more conversions and revenue over time.</p>
+                    <div>
+                        <ul class="list-disc pl-[20px] flex flex-col gap-[9px]">
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">On-page SEO</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Off-page SEO</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Technical SEO</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Local SEO</li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Illustration -->
+                <div>
+                    <img src="@/assets/seoIllustration.png" alt="Profile" class="xl:w-[489px] lg:w-[378px] w-[240px] h-auto" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
