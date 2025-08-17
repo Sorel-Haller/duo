042c4fb05a16ada17c5189d932954aa7c006cef1
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 16:34:28 +0300

    Added Footer section Admin text + responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
             Have an idea? <span class="font-normal underline underline-offset-10 decoration-[2px]">Let’s Talk</span>
         </h1>        
     </section>
-    <div class="flex flex-row justify-between gap-30 pt-12 pb-8">
+    <div class="flex flex-col md:flex-row justify-between gap-30 pt-12 pb-8">
       <!-- Contact Info -->
       <div class="flex flex-col w-[273px] h-[284px] gap-5">
         <div class="flex flex-col gap-2">
@@ -26,15 +26,30 @@
       <!-- Pages -->
       <div class="w-[140px] flex flex-col gap-[17px]">
         <h5 class="font-[Outfit] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Pages</h5>
-        <ul class="flex flex-col gap-[12px]">
-          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Home</a></li>
-          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Service</a></li>
-          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Case Study</a></li>
-          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">About Us</a></li>
-          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Blog</a></li>
-          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Contact</a></li>
+        <ul class="flex flex-col gap-[12px] text-nowrap">
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Home</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Service</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Case Study</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">About Us</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Blog</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Contact</a></li>
         </ul>
       </div>      
+      
+      <!-- Admin -->
+      <div class="w-[140px] flex flex-col gap-[17px]">
+        <h5 class="font-[Outfit] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Admin</h5>
+        <ul class="flex flex-col w-full gap-[12px] text-nowrap">
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">404</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Login</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Licenses</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Instructions</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Styleguide</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">Changelog</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[16px] md:text-[18px] leading-[23px] tracking-[-0.02em]">More Templates</a></li>
+        </ul>
+      </div>
+      
     </div>
 
   </footer>
