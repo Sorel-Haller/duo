8034efd4afc02f9b44b4f844f1b5e694eb0b3def
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 19:44:25 +0300

    Created a processService component and added it to the Service.vue

--- src/Views/Service.vue
+++ src/Views/Service.vue
@@ -1,8 +1,9 @@
 <template>
-  <OurService />
-
+    <OurService />
+    <ProcessService />
 </template>
 
 <script setup>
     import OurService from '../components/ourService.vue';
+    import ProcessService from '../components/processService.vue';
 </script>
---
+++ src/components/processService.vue
@@ -0,0 +1,6 @@
+<template>
+        <div class="pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
+            <div class="h-[500px] bg-[#004B49] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
+            </div>
+    </div>
+</template>
