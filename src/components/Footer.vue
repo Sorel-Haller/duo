57b3e3c7883483b3963c370f8acc483148b8c01c
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 16:21:19 +0300

    Added footer section Pages text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,23 +6,37 @@
             Have an idea? <span class="font-normal underline underline-offset-10 decoration-[2px]">Let’s Talk</span>
         </h1>        
     </section>
-    <div class="grid grid-cols-1 md:grid-cols-4 gap-12">
+    <div class="flex flex-row justify-between gap-30 pt-12 pb-8">
       <!-- Contact Info -->
       <div class="flex flex-col w-[273px] h-[284px] gap-5">
         <div class="flex flex-col gap-2">
-            <h3 class="font-[Outfit] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Get in touch</h3>
-            <p class="font-[Outfit] font-semibold text-[32px] leading-[42px] tracking-[-0.02em]">info@template.com</p>            
+            <h5 class="font-[Outfit] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Get in touch</h5>
+            <h3 class="font-[Outfit] font-semibold text-[32px] leading-[42px] tracking-[-0.02em]">info@template.com</h3>            
         </div>
         <div class="flex flex-col gap-3">
           <p class="font-[Supreme] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Phone</p>
           <p class="font-[Supreme] font-medium text-[18px] leading-[23px] tracking-[-0.02em]">+000 111 222 3333 <br>+444 555 666 7777</p>
         </div>
         <div class="flex flex-col gap-3">
-          <p class="font-[Supreme] font-bold text-[20px] leading-[26px] tracking-[-0.02em]">Address</p>
-          <p class="font-[Supreme] font-bold text-[18px] leading-[23px] tracking-[-0.02em]">2352 Middlefield Rd, Markham <br> Ontario, Canada</p>
+          <p class="font-[Supreme] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Address</p>
+          <p class="font-[Supreme] font-medium text-[18px] leading-[23px] tracking-[-0.02em]">2352 Middlefield Rd, Markham <br> Ontario, Canada</p>
         </div>
       </div>
+
+      <!-- Pages -->
+      <div class="w-[140px] flex flex-col gap-[17px]">
+        <h5 class="font-[Outfit] font-medium text-[20px] leading-[26px] tracking-[-0.02em]">Pages</h5>
+        <ul class="flex flex-col gap-[12px]">
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Home</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Service</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Case Study</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">About Us</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Blog</a></li>
+          <li><a href="#" class="hover:text-[#008576] font-[Supreme] font-normal text-[18px] leading-[23px] tracking-[-0.02em]">Contact</a></li>
+        </ul>
+      </div>      
     </div>
+
   </footer>
 </template>
 
