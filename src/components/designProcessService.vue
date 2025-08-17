b8828f63236d4b56ebe95e3f055190143e0f58f7
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 20:59:28 +0300

    Added main Process heading text

--- src/components/designProcessService.vue
+++ src/components/designProcessService.vue
@@ -2,7 +2,29 @@
         <div class="pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
             <div class="flex flex-col gap-[40px] bg-[#E5F6F4] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
                 <p class="font-supreme font-bold text-[#006458] text-[14px]">Our UX Design Process</p>
-                
+                <!-- User Experience -->
+                <div class="flex flex-col gap-[60px] sm:gap-[95px]">
+                    <!-- Main Process -->
+                    <div class="flex flex-col gap-[40px]">
+                        <!-- UX -->
+                        <div>
+                            <h2>User Experience</h2>
+                            <p>UX refers to the value that you offer to your users when they are using your product. It primarily depends on enhancing user satisfaction by improving the accessibility, usability and convenience when a user interacts with your product. 
+
+A well-executed UX process requires you to follow a user-centric approach and a defined strategy that enables you to develop simple, engaging and intuitive experiences to wow the end-users.
+
+Additionally, a meaningful user experience allows you to define customer journeys on your product that are most conducive to business success.
+
+Here are the UX design Process in 5 Stages : </p>
+                        </div>
+                        <!-- Illustration -->
+                        <div></div>
+                    </div>
+                    <!-- UX design Process List -->
+                    <div class="flex flex-col gap-[60px]">
+
+                    </div>
+                </div>
             </div>
     </div>
 </template>
