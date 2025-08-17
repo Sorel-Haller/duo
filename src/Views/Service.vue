fc720a678d0b98a17f45d826357a689009d6330f
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 22:08:20 +0300

    Created designService component and added it to the Service.vue view. Added title and paragraph text plus styled it and added responsive

--- src/Views/Service.vue
+++ src/Views/Service.vue
@@ -2,11 +2,13 @@
     <OurService />
     <WorkProcessService />
     <DesignProcessService />
+    <DesignService />
 </template>
 
 <script setup>
     import OurService from '../components/ourService.vue';
     import WorkProcessService from '../components/workProcessService.vue';
     import DesignProcessService from '../components/designProcessService.vue';
+    import DesignService from '../components/designService.vue';
 
 </script>
---
+++ src/components/designService.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
+        <div class="bg-[#CCEDE9] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
+            <div class="flex flex-col gap-[40px]">
+                <!-- Body -->
+                <div class="flex flex-col gap-[35px]">
+                    <h2 class="font-outfit font-semibold text-[#004B49] lg:text-[64px] sm:text-[48px] text-[36px]">Design</h2>
+                    <p class="font-outfit font-regular text-[#004B49] sm:text-[20px] text-[18px]">Designing digital products has been primary focus and a crucial part of our agency. With the business goals in mind we go in-depth to draw user’s attention, presenting your business in a meaniful way to be functional that will generate more leads.</p>
+                </div>
+                <!-- Illustration -->
+                <div>
+                    <img src="@/assets/designIllustration.png" alt="Profile" class="xl:w-[447px] lg:w-[378px] w-[240px] h-auto" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
