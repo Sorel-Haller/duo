05f9e7668729565ce5f8e943b54e64cc11f42974
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 22:16:52 +0300

    Added service list text, styled it + responsive

--- src/components/designService.vue
+++ src/components/designService.vue
@@ -6,6 +6,14 @@
                 <div class="flex flex-col gap-[35px]">
                     <h2 class="font-outfit font-semibold text-[#004B49] lg:text-[64px] sm:text-[48px] text-[36px]">Design</h2>
                     <p class="font-outfit font-regular text-[#004B49] sm:text-[20px] text-[18px]">Designing digital products has been primary focus and a crucial part of our agency. With the business goals in mind we go in-depth to draw user’s attention, presenting your business in a meaniful way to be functional that will generate more leads.</p>
+                    <div>
+                        <ul class="list-disc pl-[20px] flex flex-col gap-[9px]">
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">App Design</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Website Design</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">User Interface Design</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">User Interface Design</li>
+                        </ul>
+                    </div>
                 </div>
                 <!-- Illustration -->
                 <div>
