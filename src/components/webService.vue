d1dbd15b0257f614a9f8941bcc6883c4c9f216ba
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 22:55:56 +0300

    Created webService component and added it to service.vue in view and added style and responsive

---
+++ src/components/webService.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
+        <div class="bg-[#E5F6F4] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
+            <div class="flex xl:flex-row flex-col gap-[40px] xl:items-start">
+                <!-- Illustration -->
+                <div>
+                    <img src="@/assets/webIllustration.png" alt="Profile" class="xl:w-[489px] lg:w-[378px] w-[240px] h-auto" />
+                </div>                
+                <!-- Body -->
+                <div class="flex flex-col gap-[35px] xl:w-[1053px]">
+                    <h2 class="font-outfit font-semibold text-[#004B49] lg:text-[64px] sm:text-[48px] text-[36px]">Web Development</h2>
+                    <p class="font-outfit font-regular text-[#004B49] sm:text-[20px] text-[18px]">Our expert developers solve complex problems on the web. When we hand-over the design assets to them, they make sure the codes are efficient, highly organized and readable. From creating custom websites to using industry-leading platforms such as Webflow, Wordpress and Shopify to build powerful solutions to keep your website fast, secure and seamlessly integrated.</p>
+                    <div class="flex sm:flex-row flex-col lg:flex-nowrap flex-wrap gap-[40px]">
+                        <ul class="list-disc sm:text-nowrap xl:w-[400px] lg:w-[480px] md:w-[507px] sm:w-[300px] pl-[20px] flex flex-col gap-[9px]">
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Front-end Development</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Back-end Development</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">QA & Maintenance</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">CMS & CRM</li>
+                        </ul>
+                        <ul class="list-disc sm:text-nowrap sm:w-[300px] pl-[20px] flex flex-col gap-[9px]">
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">E-Commerce Website</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Webflow Website</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Wordpress Website</li>
+                            <li class="font-supreme font-medium text-[#006458] text-[24px]">Shopify Website</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
