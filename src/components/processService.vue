fbd8e3603e9775675c5df146907ed18fd1ae8e0d
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 20:21:00 +0300

    Added Our Work Process slogan and illustration. Added style and responsive to both.

--- src/components/processService.vue
+++ src/components/processService.vue
@@ -1,6 +1,12 @@
 <template>
         <div class="pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
-            <div class="h-[500px] bg-[#004B49] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
+            <div class="flex flex-col bg-[#004B49] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
+                <div class="flex justify-center items-center lg:flex-row flex-col gap-[10px]">
+                    <h2 class="font-outfit font-medium xl:w-[1200px] lg:w-[679px] md:text-[36px] sm:text-[36px] text-[24px] text-white">If you have no idea how to make your business unique, it’s ok, because we don’t either, but we have a method for figuring that out. We are going to collaborate every step of the way to create a unique online identity that stands out from the noise.</h2>
+                    <div class="sm:w-[350px] sm:h-[350px] flex justify-center items-center">
+                        <img src="@/assets/Illustration.png" alt="Profile" class="w-[316px] h-auto" />
+                    </div>
+                </div>
             </div>
     </div>
 </template>
