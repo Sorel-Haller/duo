be7163166e9bbc1b221e81e9723215da30868a5e
Sorel-Haller <hallersorel@gmail.com>
2025-08-17 18:39:47 +0300

    Created ourService component and Added title and service list text to it

---
+++ src/components/ourService.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px] h-[549px]">
+        <div class="bg-[#E5F6F4] pt-[40px] pb-[60px] sm:pr-[40px] pr-[20px] sm:pl-[40px] pl-[20px]">
+            <div class=" flex flex-col gap-[40px]">
+                <h1 class="text-[#006458] font-outfit font-semibold md:text-[64px] sm:text-[48px] text-[36px] text-nowrap">Our Services</h1>
+                <div class="flex sm:flex-row flex-col gap-[40px]">
+                    <div>
+                        <ul>
+                            <li>UI/UX</li>
+                            <li>App Design</li>
+                            <li>Web Design</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>SEO</li>
+                            <li>App Development</li>
+                            <li>Web Development</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
