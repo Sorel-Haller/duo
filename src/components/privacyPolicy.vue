44669d5bd436d4e95b372769fff1bf398b25e402
Sorel-Haller <hallersorel@gmail.com>
2025-08-19 20:05:45 +0300

    Added privacy policy image and added responsive to it

--- src/components/privacyPolicy.vue
+++ src/components/privacyPolicy.vue
@@ -2,6 +2,14 @@
     <div class="bg-white pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
         <div class="flex flex-col gap-[40px]">
             <h1 class="font-outfit font-regular text-[#004B49] text-[36px] sm:text-[48px] md:text-[54px] lg:text-[64px]">Privacy Policy</h1>
+            <div>
+                <div class="flex flex-col">
+                    <div class="pt-[4px] pb-[4px] pl-[4px]">
+                        <img src="https://picsum.photos/1200/600" alt="Random image" class="w-full h-auto" />
+                    </div>
+                    <div></div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
