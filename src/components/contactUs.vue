0314001fc840b5beec9d840e0db652bf1487549a
Sorel-Haller <hallersorel@gmail.com>
2025-08-19 21:24:10 +0300

    Added Please tell us a bit about your projec and textareat to contact list + fixed focus:bg color

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white pt-10 pb-0 xl:pr-[140px] md:pr-20 sm:pr-8 pr-5 xl:pl-[140px] md:pl-20 sm:pl-8 pl-5">
+    <div class="bg-white pt-10 pb-15 xl:pr-[140px] md:pr-20 sm:pr-8 pr-5 xl:pl-[140px] md:pl-20 sm:pl-8 pl-5">
         <div class="flex flex-col gap-15">
             <h1 class="font-[outfit] font-normal text-[#004B49] text-[36px] sm:text-[48px] md:text-[54px] lg:text-[64px]">Hey! Tell us about your project</h1>
             <div class="flex flex-col lg:flex-row gap-10">
@@ -8,19 +8,25 @@
                     <div class="flex flex-col gap-3">
                         <label class="block text-4 font-[Supreme] font-normal text-[#818EB0]">Full Name</label>
                         <input type="text" placeholder="John Rambo"
-                            class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-2 focus:ring-[#1a7369]" />
+                            class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-1 focus:ring-[#1a7369] focus:bg-white" />
                     </div>
                     <div class="flex flex-col gap-3">
                         <label class="block text-4 font-[Supreme] font-normal text-[#818EB0]">Email Address</label>
-                        <input type="text" class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-2 focus:ring-[#1a7369]" />
+                        <input type="text" class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-1 focus:ring-[#1a7369] focus:bg-white" />
                     </div>
                     <div class="flex flex-col gap-3">
                         <label class="block text-4 font-[Supreme] font-normal text-[#818EB0]">Company Name</label>
-                        <input type="text" class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-2 focus:ring-[#1a7369]" />
+                        <input type="text" class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-1 focus:ring-[#1a7369] focus:bg-white" />
                     </div>
                     <div class="flex flex-col gap-3">
                         <label class="block text-4 font-[Supreme] font-normal text-[#818EB0]">Project Budget (USD)</label>
-                        <input type="text" class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-2 focus:ring-[#1a7369]" />
+                        <input type="text" class="w-full h-16 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-1 focus:ring-[#1a7369] focus:bg-white" />
+                    </div>
+                    <div class="flex flex-col gap-3">
+                        <label class="block text-4 font-[Supreme] font-normal text-[#818EB0]">Please tell us a bit about your project</label>
+                        <textarea 
+                            class="w-full h-50 border border-[#00A693] bg-[#E5F6F4] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-1 focus:ring-[#1a7369] resize-none focus:bg-white"
+                        ></textarea>
                     </div>
 
                     <div class=" border-b-[0.5px] pt-4 pb-4 bg-[#00A693] hover:bg-[#1a7369] w-full text-center ">
