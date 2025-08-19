408e6ca920e3750e801797e59079f65e23501b78
Sorel-Haller <hallersorel@gmail.com>
2025-08-19 19:26:01 +0300

    Created capabilitiesService component added it to Service.vue view. Added Heading text and experience card icon and text.

---
+++ src/components/capabilitiesService.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="bg-white pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
+        <div>
+            <div class="flex flex-col gap-[80px] xl:items-center">
+                <!-- Heading -->
+                <div class="flex flex-col gap-[20px]">
+                    <h5 class="font-outfit font-bold text-[#33B8A9] text-[20px] text-center">Our Core Capabilities</h5>
+                    <h2 class="font-outfit font-medium text-[#004B49] lg:text-[64px] md:text-[54px] sm:text-[48px] text-[32px] text-center">Our capabilities and expertise makes us stand out from the noise</h2>
+                </div>
+                <!-- Service List -->
+                <div class="flex flex-col gap-[32px]">
+                    <div class="flex flex-col md:flex-row gap-[32px]">
+                        <div class="bg-[#E5F6F4] flex flex-col gap-[32px] px-[24px] py-[40px]">
+                            <div>
+                                <img src="@/assets/Icon-Experience.png" alt="Profile" class="w-[80px] h-auto" />
+                            </div>
+                            <div class="flex flex-col gap-[16px]">
+                                <h3 class="font-outfit font-semibold text-[#006458] text-[32px] text-nowrap">
+                                    User Experience
+                                </h3>
+                                <ul class="flex flex-col gap-[12px]">
+                                    <li class="font-supreme font-regular text-[#006458] text-[18px]">Sitemaps</li>
+                                    <li class="font-supreme font-regular text-[#006458] text-[18px]">User Persona</li>
+                                    <li class="font-supreme font-regular text-[#006458] text-[18px]">User Journey</li>
+                                    <li class="font-supreme font-regular text-[#006458] text-[18px]">Wireframes</li>
+                                    <li class="font-supreme font-regular text-[#006458] text-[18px]">A/B Testing</li>
+                                    <li class="font-supreme font-regular text-[#006458] text-[18px]">Design Systems</li>
+                                    <li class="font-supreme font-regular text-[#006458] text-[18px]">Copy Writing/Editing & CTAs</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="bg-[#E5F6F4] flex flex-col gap-[32px] px-[24px] py-[40px]">
+
+                        </div>
+                        <div class="bg-[#E5F6F4] flex flex-col gap-[32px] px-[24px] py-[40px]">
+                            
+                        </div>
+                    </div>
+                    <div class="flex flex-col md:flex-row gap-[32px]">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
