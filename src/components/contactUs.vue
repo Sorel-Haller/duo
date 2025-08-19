1f138fbeca1ac74b8b88d6a73c72514f68a755e5
Sorel-Haller <hallersorel@gmail.com>
2025-08-19 21:09:13 +0300

    Added Full name text and imput to contact form + added button, styled it and added responsive

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,9 +1,24 @@
 <template>
-    <div class="bg-white pt-[40px] pb-[40px] xl:pr-[140px] md:pr-[80px] sm:pr-[32px] pr-[20px] xl:pl-[140px] md:pl-[80px] sm:pl-[32px] pl-[20px]">
-        <div class="flex flex-col gap-[60px]">
-            <h1 class="font-outfit font-regular text-[#004B49] text-[36px] sm:text-[48px] md:text-[54px] lg:text-[64px]">Hey! Tell us about your project</h1>
-            <div>
+    <div class="bg-white pt-10 pb-0 xl:pr-[140px] md:pr-20 sm:pr-8 pr-5 xl:pl-[140px] md:pl-20 sm:pl-8 pl-5">
+        <div class="flex flex-col gap-15">
+            <h1 class="font-[outfit] font-normal text-[#004B49] text-[36px] sm:text-[48px] md:text-[54px] lg:text-[64px]">Hey! Tell us about your project</h1>
+            <div class="flex flex-col lg:flex-row gap-10">
+                <!-- Contact form -->
+                 <div class="flex flex-col gap-[60px] w-full">
+                    <div class="flex flex-col gap-3">
+                        <label class="block text-4 font-[Supreme] font-normal text-[#818EB0]">Full Name</label>
+                        <input type="text" placeholder="John Rambo"
+                            class="w-full h-16 border border-[#00A693] text-[#030024] p-5 rounded-sm focus:outline-none focus:ring-2 focus:ring-[#1a7369]" />
+                    </div>
 
+                    <div class=" border-b-[0.5px] pt-4 pb-4 bg-[#00A693] hover:bg-[#1a7369] w-full text-center ">
+                        <button class="font-[Neue Haas Grotesk Display Pro] font-medium text-6 ">Submit</button>
+                    </div>
+                 </div>
+                <!-- Contact info -->
+                 <div class="flex flex-col gap-[40px] w-full">
+                    
+                 </div>
             </div>
         </div>
     </div>
